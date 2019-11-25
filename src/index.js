db58584df8a8032ca000c072c18de018609f85eb
maggithor97 <maggithor1@gmail.com>
2019-11-25 14:13:28 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,13 +16,51 @@ document.addEventListener('DOMContentLoaded', () => {
   const isLecturePage = page.classList.contains('lecture-page');
   event.preventDefault();
 
-  if (isLecturePage) {
+  if (isLecturePage) {  
+    //  Fetchar og lætur búa til fyrirlesturs síðu
+    fetch('lectures.json')
+    .then((result) => {
+      if (!result.ok) {
+        throw new Error('Non 200 status');
+      }
+      return result.json();
+    })
+    .then(data => buaTilFyrirlestur(data, fyrirlesturNumer))
+    .catch(error => console.error(error));
+
   } else {
-    //const list = new List();
-    //list.load();
-    console.log('Else');
+    
+    const list = new List();
+    list.load();
+
+    //  Fetchar og lætur búa til index síðu
+    fetch('lectures.json')
+  .then((result) => {
+    if (!result.ok) {
+      throw new Error('Non 200 status');
+    }
+    return result.json();
+  })
+  .then(data => buaTilForsidu(data))
+  .catch(error => console.error(error));
   }
 });
+
+//  Hérna búum við til forsíðuna
+/*
+function buaTilForsidu(data) {
+  console.log(data.lectures[0]);
+
+}
+
+*/
+//  Hérna búum við til fyrirlestur
+function buaTilFyrirlestur(data, i) {
+  console.log(data.lectures[i]);
+}
+
+
+
 
 // add event listeners for the buttons
 for (let i = 0; i < 3; i++)
@@ -51,3 +89,4 @@ function clickHandler(btntype) {
   }
 
 }
+  
